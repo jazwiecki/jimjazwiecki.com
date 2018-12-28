c3a8a359cfad686b4b1dc20e5118378ce49c5aef
Jim Jazwiecki <jim.jazwiecki@gmail.com>
2018-12-28 01:03:57 -0500

    breaking grid in the header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,22 +9,14 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="jazwiecki" class="logo" />
           </figure>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
         </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/jazwiecki"
           target="_blank"
           rel="noopener noreferrer"
         >
