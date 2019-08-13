44bbcd9688c80b42b8e8f2a1509aecd5b49cf529
Jim Jazwiecki <jim.jazwiecki@gmail.com>
2019-08-12 21:47:56 -0400

    PoC jest test (#39)

---
+++ src/components/__tests__/navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import renderer from "react-test-renderer"
+
+import Navbar from "../navbar"
+
+describe("Navbar", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(<Navbar />)
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+});
