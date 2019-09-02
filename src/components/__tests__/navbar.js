c1be39a02900845f2dd4893ead2db702c72c1163
Jim Jazwiecki <jim.jazwiecki@gmail.com>
2019-09-01 22:56:18 -0400

    maybe case-sensitivity?

--- src/components/__tests__/navbar.js
+++ src/components/__tests__/navbar.js
@@ -1,7 +1,7 @@
 import React from "react"
 import renderer from "react-test-renderer"
 
-import Navbar from "../navbar"
+import Navbar from "../Navbar"
 
 describe("Navbar", () => {
   it("renders correctly", () => {
