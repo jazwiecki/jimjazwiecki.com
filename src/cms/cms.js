12bcdefbb18dc224c18eb98e046905e8ccc6bd79
Jim Jazwiecki <jim.jazwiecki@gmail.com>
2020-04-25 17:02:53 -0400

    Upgrade to netlify-cms-app (#556)
    * switch from yarn to npm for package locking (removed yarn.lock)
    * switch to netlify-cms-app

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,9 @@
-import CMS from 'netlify-cms'
+import CMS from 'netlify-cms-app'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
+
+CMS.init();
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
